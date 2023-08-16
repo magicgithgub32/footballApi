771365aa4ad26dc3a32c2137e3d28c1b22fe4859
Rubcs <Rubcs@gmail.com>
2023-08-16 15:25:24 +0200

    add prev and next  buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,9 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  const hanleInputChange = () => {
-    return;
-  };
-
   return (
     <header>
       <h1>PREMIER LEAGUE CLUBS & THEIR LOGOS</h1>
-      <input
-        className="searchInput"
-        type="text"
-        placeholder="Search your club"
-        //   value=
-        onChange="handleInputChange"
-      />
-      <button type="submit" className="searchButton" onClick="handleClick">
-        Search{" "}
-      </button>
     </header>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./Hero.css";
 
 const Hero = () => {
+  const [standingsData, setStandingsData] = useState(null);
   const [footballTeam, setFootballTeam] = useState("");
   const [teamLogo, setTeamLogo] = useState("");
   const [teamId, setTeamId] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const getFootball = async () => {
     const result = await fetch(
@@ -13,26 +15,96 @@ const Hero = () => {
 
     if (result.status === 200) {
       const res = await result.json();
-      setFootballTeam(res.data.standings);
+      setStandingsData(res.data.standings);
+      setFootballTeam(standingsData[teamId]?.team.name);
+      setTeamLogo(standingsData[teamId]?.team.logos[0].href);
     } else {
       setFootballTeam(null);
+      setTeamLogo("");
     }
   };
 
   useEffect(() => {
     getFootball();
-  }, [footballTeam]);
+  }, [standingsData]);
+
+  const getPreviousTeam = () => {
+    setTeamId(teamId - 1);
+  };
+
+  const getNextTeam = () => {
+    setTeamId(teamId + 1);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+    event.preventDefault();
+  };
+
+  const handleClick = () => {
+    if (standingsData) {
+      const foundTeam = standingsData.find(
+        (team) => team.team.name.toLowerCase() === searchQuery.toLowerCase()
+      );
+
+      if (foundTeam) {
+        console.log("foundTeam", foundTeam);
+        setTeamId(foundTeam.stats[0].rank - 1);
+        setFootballTeam(foundTeam.team.displayName);
+        setTeamLogo(foundTeam.team.logos[0].href);
+      } else {
+        setFootballTeam(null);
+        setTeamLogo("");
+      }
+    }
+  };
 
   return (
     <>
       {footballTeam && (
-        <div className="hero-wrapper">
-          <h2>{footballTeam[teamId].team?.name}</h2>
-          <img
-            className="logo"
-            src={footballTeam[teamId].team?.logos[0].href}
-          />
-        </div>
+        <section className="hero-wrapper">
+          <article className="search-buttons">
+            <input
+              className="search-input"
+              type="text"
+              placeholder="Search your club"
+              value={searchQuery}
+              onChange={handleSearchInputChange}
+            />
+            <button
+              type="submit"
+              className="search-button"
+              onClick={handleClick}
+            >
+              Go!{" "}
+            </button>
+          </article>
+          <article className="name-logo-wrapper">
+            <h2 className="team-name">{footballTeam}</h2>
+            <img className="logo" src={teamLogo} />
+          </article>
+          <article className="next-prev-buttons">
+            {teamId > 0 && (
+              <button
+                type="submit"
+                className="prevButton"
+                onClick={getPreviousTeam}
+              >
+                Prev
+              </button>
+            )}
+
+            {teamId < 19 && (
+              <button
+                type="submit"
+                className="nextButton"
+                onClick={getNextTeam}
+              >
+                Next
+              </button>
+            )}
+          </article>
+        </section>
       )}
     </>
   );
